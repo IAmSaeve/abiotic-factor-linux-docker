614a1ea6c5f1a6d45651e4700cd6d47979fc521c
Count Vonn <xenoid_66@hotmail.com>
2025-09-30 16:47:59 -0400

    Handle state is 0x6 when updating server binaries

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
+app_update() {
+    steamcmd \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir /server \
+    +login anonymous \
+    +app_update 2857200 validate \
+    +quit
+}
+
 SetUsePerfThreads="-useperfthreads "
 if [[ $UsePerfThreads == "false" ]]; then
     SetUsePerfThreads=""
@@ -18,12 +27,13 @@ AdditionalArgs="${AdditionalArgs:-}"
 
 # Check for updates/perform initial installation
 if [ ! -d "/server/AbioticFactor/Binaries/Win64" ] || [[ $AutoUpdate == "true" ]]; then
-    steamcmd \
-    +@sSteamCmdForcePlatformType windows \
-    +force_install_dir /server \
-    +login anonymous \
-    +app_update 2857200 validate \
-    +quit
+    app_update | tee /tmp/app_update.log
+    grep -q "state is 0x6" /tmp/app_update.log
+    if [ $? -eq 0 ]; then
+        echo "Update failed with 'state is 0x6', wiping server files and retrying (your saved games will be preserved)"
+        find /server/* -not -path "/server/AbioticFactor/Saved*" -not -path "/server/AbioticFactor" -delete
+        app_update
+    fi
 fi
 
 pushd /server/AbioticFactor/Binaries/Win64 > /dev/null
